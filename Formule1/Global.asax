570a13138fe768b9013a0bf6c94262a4662b32b1
Rico Kolditz <ricokolditz@hotmail.com>
2014-05-09 12:28:45 +0200

    First commit , startup

---
+++ Formule1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Formule1.MvcApplication" Language="C#" %>
---
+++ Formule1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Formule1.Startup))]
+namespace Formule1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
